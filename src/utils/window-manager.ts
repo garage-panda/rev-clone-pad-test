778a209b4945c54d9c0d6159c499be6373de8c43
Angel Angelov <angel.angelov@accedia.com>
2022-08-31 14:52:49 +0300

    chore:  Trigger auto update in window-manager

--- src/utils/window-manager.ts
+++ src/utils/window-manager.ts
@@ -4,6 +4,7 @@ import {
   MAIN_SCREEN_CONFIG,
   withPreload,
 } from '../shared/config/screen-config';
+import AutoUpdater from './auto-updater';
 
 declare const MAIN_WEBPACK_ENTRY: string;
 declare const MAIN_PRELOAD_WEBPACK_ENTRY: string;
@@ -11,20 +12,23 @@ declare const LOADING_WEBPACK_ENTRY: string;
 declare const LOADING_PRELOAD_WEBPACK_ENTRY: string;
 
 class WindowManager {
+  private autoUpdater: AutoUpdater;
   loadingWindow: BrowserWindow | null;
   mainWindow: BrowserWindow | null;
 
   constructor() {
     this.loadingWindow = null;
     this.mainWindow = null;
+    this.autoUpdater = new AutoUpdater(this.loadingWindow);
   }
 
   public async showLoadingWindow(): Promise<void> {
     const screenConfig = withPreload(LOADING_SCREEN_CONFIG, LOADING_PRELOAD_WEBPACK_ENTRY);
     this.loadingWindow = new BrowserWindow(screenConfig);
     await this.loadingWindow.loadURL(LOADING_WEBPACK_ENTRY);
-    // TODO: Auto-update logic
-    // setTimeout(() => this.showMainWindow(), 2000);
+    await this.autoUpdater.checkForUpdates();
+
+    this.showMainWindow();
   }
 
   public async showMainWindow(): Promise<void> {
